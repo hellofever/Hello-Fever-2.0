fcd24c167e83c5d3449a485d02b62d0aa0621fd9
Paolo Sta. Barbara <paolostabarbara@gmail.com>
2013-06-18 01:02:33 +1000

    Final

--- js/main.js
+++ js/main.js
@@ -1,18 +1,75 @@
-// //JS Document
+/**
+ * jQuery.browser.mobile (http://detectmobilebrowser.com/)
+ *
+ * jQuery.browser.mobile will be true if the browser is a mobile device
+ *
+ **/
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
+
+//JS Document
 $(document).ready(function() {
 
-	//MOBILE MENU
+	
+	//------------------------------------
+	//
+	//	MOBILE MENU
+	//
+	//------------------------------------
+
 	$('#menuToggle').click(function() {
 		//menu button reveals up and down
 		$('#mainNav').slideToggle('fast');
 		//add styling with class style close
 		$('#menuToggle').toggleClass('close')
 	});
-	//End mobile menu
+
+	//	End mobile menu
+
+	//------------------------------------
+	//
+	//	MAGNIFIC POPUP
+	//
+	//------------------------------------
+
+	$(document).ready(function() {
+        $('.popup-vimeo').magnificPopup({
+          disableOn: 700,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: true,
+
+          fixedContentPos: false
+        });
+      });
+
+	//------------------------------------
+	//
+	//	SCROLL TO TOP
+	//
+	//------------------------------------
+	$('.active').click(function(){
+	    $("html, body").animate({ scrollTop: 0 }, 1000);
+	    return false;
+    });
+
+
+
+
+
+
+	if(!jQuery.browser.mobile){
+
+	
+	   
+	//------------------------------------
+	//
+	//	WORK SLIDER
+	//
+	//------------------------------------
 
 	//add functionality to the rollover work section
-	
 	//declare project variable and count how many articles
 	var $proj = $('#workSlider > article');
 	var $projTotal = $('#workSlider article').length;
@@ -22,45 +79,41 @@ $(document).ready(function() {
 	//create mouse over handler for each article
 	var mouseoverHandler = function (e) {
 		//adjust size of highlight here
-		var highlightSize = 30;
+		var highlightSize = 35;
 		//divide the rest of the sizes here
 		var otherSize = (100 - highlightSize) / ($projTotal - 1);
 
 		//on highlighted article, increase width to highlightSize;
 		$(this).css('width', highlightSize + '%')
-		//$('img',this).addClass('move')
-		
+
 		//Toggle off class
 		$proj.not(this).css('width', otherSize + '%')
 			 .addClass('off');
 
 		//add mouse tracking functinality 
 		$proj.mousemove(trackPosition);
+
 	};
 
 	//track mouse position
 	var trackPosition = function (e) {
 		//set the offset of current element
 		var parentOffset = $(this).offset();
+		
 		//X/Y pos is mouse position - offset
 		var relX = e.pageX - parentOffset.left;
    		var relY = e.pageY - parentOffset.top;
-
-   		console.log('relX = ' + relX);
 
    		//divide image width by element width
    		$projWidth = $(this).css('width');
    		$projImg = $('img', this).css('width');
 
+   		var moveSpeed = (parseInt($projImg) / parseInt($projWidth)) / 10;
+
 		//change imgpos left to match offset
-		moveImage = ((parseInt($projImg) / parseInt($projWidth)) * relX * -1)*0.4;
-		console.log('moveImage = ' + moveImage)
+		var moveImage = moveSpeed * relX * -1;
 
-		$('img', this).css('left', moveImage);
-
-
-   		// //var mouseUnit = parseInt($projWidth / $projImg);
-   		// console.log('mouseUnit = ' + parseInt($projImg));
+		//$('img', this).css('left', moveImage);
 	};
 
 	//revert on mouse out
@@ -71,14 +124,57 @@ $(document).ready(function() {
 		$proj.css('width', returnSize + '%')
 			 .removeClass('off');
 
-		$('img', this).css('left', '0');
-
-		//remove class to move image back
-		//$('img',this).removeClass('move')
+		//position img
+		$('img', this).css('left', '-60%');
 	};
 
 	// perform mouseover fucntion
 	$proj.mouseover(mouseoverHandler);
 	$proj.mouseleave(mouseoutHandler)
 
+	// END WORK SLIDER 
+
+
+	//------------------------------------
+	//
+	//	HEADER MOUSEOVER FUNCTIONALITY
+	//
+	//------------------------------------
+
+
+	//make scroll top a var and create vars for elements to be edited
+	
+	var $header = $('header');
+	var $headerHeight = $header.css('height');
+	var $h1 = $('h1');
+	var $a = $('header nav.main a');
+
+	//check menu status
+	var animateIn = function (state) {
+		$header.addClass('init');
+		$a.addClass('init');
+	}
+
+	animateIn();
+	}
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
